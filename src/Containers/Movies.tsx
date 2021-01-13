f3d0f635eb7194fbdfdaa67920fc727aa99aace4
Kerem <krmtrk20@gmail.com>
2021-01-13 11:32:59 +0300

    add route components to ensure routing is working

---
+++ src/Containers/Movies.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+export const Movies: React.FC = () => {
+    return (
+        <Typography variant="h1">
+                Movies page it is!
+        </Typography>
+    )
+
+}
---
+++ src/Containers/Series.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+export const Series: React.FC = () => {
+    return (
+        <h1>
+            Series page
+        </h1>
+    )
+}
