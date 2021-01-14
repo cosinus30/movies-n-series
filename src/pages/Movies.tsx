b9fa5775f31a58fd67b3e2a1c64f7b7cf446f8b9
Kerem <krmtrk20@gmail.com>
2021-01-14 15:09:01 +0300

    add rows with corresponding titles and urls

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,17 @@
+import { Grid, Typography } from "@material-ui/core";
+import Container from '@material-ui/core/Container'
+import React from "react";
+import { Card } from "../Components/Card/Card";
+import { CardRow } from "../Containers/CardRow/CardRow";
+
+export const Movies: React.FC = () => {
+    return (
+        <Container maxWidth='xl'>
+            <CardRow title="Popular Movies" url="popular"/>
+            <CardRow title="Trending Movies"/>
+            <CardRow title="Top-rated Movies" url="top_rated"/>
+            <CardRow title="Now Playing" url="now_playing"/>
+            <CardRow title="Upcoming Movies" url="upcoming"/>
+        </Container>
+    );
+};
