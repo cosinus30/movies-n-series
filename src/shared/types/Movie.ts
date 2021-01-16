e19a04d0648b5b6cabf15c5f503f20d3ec32966a
Kerem <krmtrk20@gmail.com>
2021-01-16 12:21:35 +0300

    add new types for movie detail response

--- src/shared/types/Movie.ts
+++ src/shared/types/Movie.ts
@@ -13,6 +13,31 @@ export type Movies = {
     total_pages: number
 }
 
+export type Genre = {
+    id: number,
+    name: string
+}
+
 export type MovieDetail = {
-    
+    title: string,
+    original_title: string,
+    release_date: string,
+    overview: string,
+    popularity: number,
+    genres: Genre[],
+    vote_average: number,
+    tagline: string,
+    backdrop_path: string,
+}
+
+export type Person = {
+    name: string,
+    profile_path: string,
+    character: string,
+    id: number,
+}
+
+export type Credits = {
+    cast: Person[],
+    crew: Person[],
 }
