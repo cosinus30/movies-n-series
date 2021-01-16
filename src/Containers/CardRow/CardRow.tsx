8ed27e91df00ae932b139e8594ac499758dca5dd
Kerem <krmtrk20@gmail.com>
2021-01-16 12:31:42 +0300

    refactor the way of retrieving data
    first time I use react-query yey!

--- src/Containers/CardRow/CardRow.tsx
+++ src/Containers/CardRow/CardRow.tsx
@@ -1,13 +1,18 @@
-import { Box, Button, createStyles, Grid, IconButton, makeStyles, Theme, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 import { ChevronLeftSharp, ChevronRightSharp } from "@material-ui/icons";
-import React, { useEffect, useRef, useState } from "react";
-import { Movies } from "../../shared/types/Movie";
-import { Card } from "../../Components/Card/Card";
+import React, { useRef } from "react";
+import { Card } from "../../Components/organisms/Card/Card";
 import { api } from "../../shared/api/api";
+import { movieListingTypes } from "../../shared/api/movie-resource";
+import { useQuery, useQueryClient } from "react-query";
 
 export type CardRowProps = {
     title: string;
-    url?: string;
+    url?: movieListingTypes;
 };
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -27,7 +32,8 @@ const useStyles = makeStyles((theme: Theme) =>
             flexWrap: "nowrap",
         },
         title: {
-            color: theme.palette.primary.light,
+            color: theme.palette.secondary.main,
+            marginLeft: theme.spacing(4.5),
         },
         titleBar: {
             background: "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
@@ -37,8 +43,15 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const CardRow: React.FC<CardRowProps> = (props) => {
     const classes = useStyles();
+    const queryClient = useQueryClient();
     const divRef = useRef<HTMLDivElement>(null);
-    const [movies, setMovies] = useState<Movies | null>(null);
+
+    const movieQuery = useQuery(
+        props.url ? props.url : "trending",
+        props.url ? () => api.movie.getMovies(props.url) : api.movie.getTrending
+    );
+
+    const { data, error, isLoading } = movieQuery;
 
     const handleScroll = (direction: string) => {
         if (divRef && divRef.current) {
@@ -50,35 +63,30 @@ export const CardRow: React.FC<CardRowProps> = (props) => {
         }
     };
 
-    useEffect(() => {
-        // if (props.url) {
-        //     api.movie.getMovies(props.url)
-        //     .then((response) => {
-        //         setMovies(response)
-        //     })
-        // }
-        // else {
-        //     api.movie.getTrending()
-        //     .then((response) => {
-        //         setMovies(response)
-        //     })
-        // }
-    }, [props.url]);
-
     return (
-        <Box marginY={6}>
-            <Typography variant="h4">{props.title}</Typography>
+        <Box marginY={6} color="textPrimary">
+            <Typography variant="h4" className={classes.title}>
+                {props.title}
+            </Typography>
             <Box className={classes.gridRow} marginY={4}>
                 <IconButton onClick={() => handleScroll("left")}>
-                    <ChevronLeftSharp fontSize="large" />
+                    <ChevronLeftSharp fontSize="large" color="secondary" />
                 </IconButton>
                 <Grid container className={classes.gridList} spacing={4} ref={divRef}>
-                    {movies?.results.map((movie) => {
-                        return <Card key={movie.id} title={movie.title} posterPath={movie.poster_path} id={movie.id} />;
+                    {data?.results.map((movie) => {
+                        return (
+                            <Card
+                                key={movie.id}
+                                title={movie.title}
+                                posterPath={movie.poster_path}
+                                id={movie.id}
+                                isLoading={isLoading}
+                            />
+                        );
                     })}
                 </Grid>
                 <IconButton onClick={() => handleScroll("right")}>
-                    <ChevronRightSharp fontSize="large" />
+                    <ChevronRightSharp fontSize="large" color="secondary" />
                 </IconButton>
             </Box>
         </Box>
