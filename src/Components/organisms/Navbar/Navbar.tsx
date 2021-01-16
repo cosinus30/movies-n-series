db02b3d7dad3e030f19703533bf018e9fc293f8e
Kerem <krmtrk20@gmail.com>
2021-01-16 12:30:39 +0300

    refactor way of styling navbar

--- src/Components/organisms/Navbar/Navbar.tsx
+++ src/Components/organisms/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Appbar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import { Box, createStyles, Link, makeStyles, Theme, Toolbar } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import Toolbar from "@material-ui/core/Toolbar";
 import { NavLink as RouterLink } from "react-router-dom";
-import "./Navbar.css";
 
 const useStyles = makeStyles((theme: Theme) => {
     return createStyles({
         navLink: {
-            fontSize: '18px',
+            fontSize: "18px",
             "&:hover": {
                 textDecoration: "none",
             },
-        }
+        },
+        toolbar: {
+            justifyContent: "center",
+        },
     });
 });
 
@@ -20,9 +24,15 @@ export const Navbar: React.FC = () => {
     const classes = useStyles();
     return (
         <Appbar position="static">
-            <Toolbar className="toolbar">
+            <Toolbar className={classes.toolbar}>
                 <Box marginX={2}>
-                    <Link className={classes.navLink} activeClassName={"Mui-selected"} color="inherit" component={RouterLink} to="/movies">
+                    <Link
+                        className={classes.navLink}
+                        activeClassName={"Mui-selected"}
+                        color="inherit"
+                        component={RouterLink}
+                        to="/movies"
+                    >
                         Movies
                     </Link>
                 </Box>
