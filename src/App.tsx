6af36e26149ed3c027ed3e73bd0959625514ea19
Kerem <krmtrk20@gmail.com>
2021-01-16 12:20:34 +0300

    add react query provider & customized material theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
 import React from "react";
-import "./App.css";
-import Button from "@material-ui/core/Button";
-import { Navbar } from "./Components/Navbar/Navbar";
+import { Navbar } from "./Components/organisms/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
-import { Movies } from "./Containers/Movies";
-import { Series } from "./Containers/Series";
+import { Movies } from "./pages/Movies";
+import { Series } from "./pages/Series";
+import { MovieDetail } from "./Containers/MovieDetail/MovieDetail";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { QueryClientProvider, QueryClient } from "react-query";
+const theme = createMuiTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 480,
+            md: 800,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+    palette: {
+        primary: {
+            main: "#141414",
+        },
+        secondary: {
+            main: "#D7D9D6",
+        },
+        text: {
+            primary: "#D7D9D6",
+        },
+    },
+});
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="App">
-                <Navbar />
-            </div>
-            <Switch>
-                <Route exact path="/movies" component={Movies} />
-                <Route exact path="/series" component={Series} />
-            </Switch>
+            <ThemeProvider theme={theme}>
+                <QueryClientProvider client={queryClient}>
+                    <div className="App">
+                        <Navbar />
+                    </div>
+                    <Switch>
+                        <Route path="/movies" component={Movies} />
+                        <Route exact path="/series" component={Series} />
+                    </Switch>
+                    <Route path={"/movies/:id"} component={MovieDetail} />
+                </QueryClientProvider>
+            </ThemeProvider>
         </BrowserRouter>
     );
 }
