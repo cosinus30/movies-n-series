41e38aa1991d7b6c83092d7b1245b083b56a8896
Kerem <krmtrk20@gmail.com>
2021-01-20 23:03:33 +0300

    add new page

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,33 +1,16 @@
-import { createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
 import React from "react";
-import { ReactComponent as UnderConstruction } from "../assets/plug.svg";
-
-const useStyles = makeStyles((theme: Theme) => {
-    return createStyles({
-        container: {
-            width: "100%",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column"
-        },
-
-        icon: {
-            width: 200,
-            height: 200,
-        },
-    });
-});
+import Typography from '@material-ui/core/Typography'
+import Container from "@material-ui/core/Container"
+import { CardRow } from "../Containers/CardRow/CardRow";
+import { api } from "../shared/api/api";
 
 export const Series: React.FC = () => {
-    const classes = useStyles();
-
     return (
-        <div className={classes.container}>
-            <UnderConstruction className={classes.icon} />
-            <br/>
-            <Typography color={"secondary"} variant={'h2'}>This page is under construction!</Typography>
-        </div>
+        <Container maxWidth="xl">
+            <CardRow title="Popular Tv Shows" url="popular" api={() => api.tv.getSeries("popular")}/>
+            <CardRow title="Trending Tv Shows" url="trending" api={() => api.tv.getTrending()}/>
+            <CardRow title="Top-rated Tv Shows" url="top_rated" api={() => api.tv.getSeries("top_rated")}/>
+            <CardRow title="On air" url="on_the_air" api={() => api.tv.getSeries("on_the_air")}/>
+        </Container>
     );
 };
